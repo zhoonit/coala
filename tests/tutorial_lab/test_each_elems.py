5a0bc3c28bbee74b5b6ca6cd4ad9bf996fa4c1a8
Jaesung Lee <ljaesung1130@gmail.com>
2026-04-20 21:55:39 +0900

    discovery(tutorial):
    Discovered volcano

--- tests/tutorial_lab/test_each_elems.py
+++ tests/tutorial_lab/test_each_elems.py
@@ -26,4 +26,8 @@ def test_fuse_fire_and_water(lab: Lab):
 def test_fuse_fire_and_earth(lab: Lab):
     result = lab.fuse("fire", "earth")
     assert result == "magma"
+
+def test_fuse_magma_and_earth(lab: Lab):
+    result = lab.fuse("magma", "earth")
+    assert result == "volcano"
 # ..
