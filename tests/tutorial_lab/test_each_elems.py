de0380179b8a42989686aa6b2d947ef09fd6c1a3
twalla26 <ssm4748@naver.com>
2026-07-05 15:28:08 +0900

    discover blaze

--- tests/tutorial_lab/test_each_elems.py
+++ tests/tutorial_lab/test_each_elems.py
@@ -21,3 +21,8 @@ def test_lab_integrity(lab: Lab):
 def test_fuse_fire_and_water(lab: Lab):
     result = lab.fuse("fire", "water")
     assert result == "steam"
+
+
+def test_fuse_fire_and_fire(lab: Lab):
+    result = lab.fuse("fire", "fire")
+    assert result == "blaze"
