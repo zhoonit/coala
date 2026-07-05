a4935324a58d44a85fc30f5b907f9c7430d1cb2c
최진우 <cjw1234577@gmail.com>
2026-07-05 12:54:56 +0900

    idscover(tutorial): add spark element - add spark element to tutorial by fusing fire + wind

--- tests/tutorial_lab/test_each_elems.py
+++ tests/tutorial_lab/test_each_elems.py
@@ -23,4 +23,11 @@ def test_fuse_fire_and_water(lab: Lab):
     assert result == "steam"
 
 
+def test_fuse_steam_and_wind(lab: Lab):
+    result = lab.fuse("steam", "wind")
+    assert result == "cloud"
+    
+def test_fuse_fire_and_wind(lab: Lab):
+    result = lab.fuse("fire", "wind")
+    assert result == "spark"
 # ..
