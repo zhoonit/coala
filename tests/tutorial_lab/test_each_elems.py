9dc26f471867fc7b96978b291bd83ee143f8955b
Kihyun <69492686+leeve1247@users.noreply.github.com>
2026-07-05 15:28:27 +0900

    Discovered Gale

--- tests/tutorial_lab/test_each_elems.py
+++ tests/tutorial_lab/test_each_elems.py
@@ -22,5 +22,7 @@ def test_fuse_fire_and_water(lab: Lab):
     result = lab.fuse("fire", "water")
     assert result == "steam"
 
-
+def test_fuse_wind_and_water(lab: Lab):
+    result = lab.fuse("wind", "wind")
+    assert result == "gale"
 # ..
